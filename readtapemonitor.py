a6b6636b3c3811e603eea64db5566772dd86f799
mjlosch <Martin.Losch@awi.de>
2020-11-20 10:05:01 +0100

    add debugging script for ad-simulations

---
+++ readtapemonitor.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-15 -*-
+######################## -*- coding: utf-8 -*-
+"""Usage: readtapemonitor.py INPUTFILE
+Simple script to read variables in monitor files as a function of tsnumber
+This is intended for debugging taf-generated code, where we can write
+multiple monitor output with the same tsnumber
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from getopt import gnu_getopt as getopt
+import sys
+
+# parse command-line arguments
+try:
+    optlist,args = getopt(sys.argv[1:], ':', ['verbose'])
+    assert len(args) < 2
+except (AssertionError):
+    sys.exit(__doc__)
+
+if len(args) > 0:
+    fname = args[0]
+else:
+    fname = 'STDOUT.0000'
+
+numbername, varname='time_tsnumber', 'dynstat_eta_min'
+#numbername, varname='seaice_tsnumber', 'seaice_heff_max'
+#numbername, varname='seaice_tsnumber', 'seaice_hsnow_max'
+niter0=464592
+nsteps=48
+tsnumber = range(niter0+nsteps,niter0,-1)
+
+def get_parms (fname):
+    rhoIce = 910.
+    with open(fname) as f:
+        for line in f:
+            if 'nIter0   =   /* Run starting timestep number */' in line:
+                nIter0 = int(next(f).strip().split()[-1])
+            elif 'nTimeSteps = /* Number of timesteps */' in line:
+                nTimeSteps = int(next(f).strip().split()[-1])
+
+    return nIter0, nTimeSteps
+
+niter0,nsteps=get_parms(fname)
+tsnumber = range(niter0+nsteps,niter0,-1)
+vars=[]
+for k,mynumber in enumerate(tsnumber):
+    print(k,mynumber)
+    var = []
+    with open(fname) as fo:
+        for line in fo:
+            if numbername in line and str(mynumber) in line:
+                # print(line)
+                while True:
+                    ll=fo.readline()
+                    # print(ll)
+                    if varname in ll:
+                        var.append(float(
+                            ll.strip().split()[-1].replace('E','e')))
+                        break
+                # # skip two lines ...
+                # next(fo)
+                # next(fo)
+                # # ... and read the third, which is dynstat_eta_min
+                # ll=fo.readline().strip().split()
+                # var.append(float(ll[-1].replace('E','e')))
+    if len(var) < 4: break
+    vars.append(var)
+
+# convert to numpy array
+etamin = np.asarray(vars)
+
+plt.clf(); plt.plot(tsnumber[:len(vars)],etamin[:,1:3],'x-')
+plt.xlabel('timestep')
+plt.ylabel(varname)
+plt.show()
+
+#grep -A6 "(PID.TID 0000.0001) %MON time_tsnumber                =                472740" STDOUT.0000 | grep eta_min
